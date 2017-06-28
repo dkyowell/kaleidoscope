9cfa26b65e519bcad320ecbdfa2dfec370814cca
David Yowell <dky@Davids-MacBook-Pro-2.local>
2017-06-28 15:40:10 -0500

    Initial Commit

---
+++ Kaleidoscope/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kaleidoscope
+//
+//  Created by David Yowell on 6/28/17.
+//  Copyright © 2017 David Yowell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
